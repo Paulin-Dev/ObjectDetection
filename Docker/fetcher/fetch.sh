9efee606a0aa7c72fcf745cc5a8a50332d778d43
Paulin-Dev <paulinrenault14@gmail.com>
2024-05-17 15:01:04 +0200

    Functions instead of lots of ugly rows & retry if image is corrupted

--- Docker/fetcher/fetch.sh
+++ Docker/fetcher/fetch.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
 
-for _ in $(seq 1 "$LOOP"); do
+filename="/tmp/CWL9018.jpg"
+retried=0
 
-    filename="/tmp/CWL9018.jpg"
+
+retry() {
+    if [ "$retried" -eq 1 ]; then
+        retried=0
+    else
+        retried=1
+        sleep 10
+        download
+    fi
+}
+
+
+download () {
+    local fileSize createDate
 
     wget -O "$filename" "$URL"
 
+    # Check if the file size if bigger than 10ko
     fileSize=$(exiftool -s -s -s -n -FileSize "$filename")
-
-    if ((fileSize > 10000)); then
+    if [ "$fileSize" -gt 10000 ]; then
         createDate=$(exiftool -s -s -s -d "%Y%m%d%H%M%S" -CreateDate "$filename")
         mv -n "$filename" "/images/$createDate.jpg"
+    else
+        retry
     fi
+}
 
+
+for _ in $(seq 1 "$LOOP"); do
+
+    download
     rm -f "$filename"
     sleep "$SLEEP_TIME"
 
